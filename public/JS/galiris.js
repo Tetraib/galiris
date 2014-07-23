8ffe4f2cb7ece2073b247b661dd1d80bbf4e5c3e
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2014-07-23 08:57:51 +0000

    no subtitle shadows

--- public/JS/galiris.js
+++ public/JS/galiris.js
@@ -1,5 +1,5 @@
 $( document ).ready(function(){
-    var backselect = Math.floor((Math.random() * 3) + 1)
+    var backselect = Math.floor((Math.random() * 3) + 1);
     $('html').css('background-image', 'url(/IMG/BACKGROUND_'+backselect+'.jpg)', 'no-repeat center center fixed');
     $('html').css('background-size', 'cover');
 });
