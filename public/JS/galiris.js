4a07a0f827c952b798f22fbd0c03cd45a15ca4c3
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2014-07-23 09:10:40 +0000

    add comments

--- public/JS/galiris.js
+++ public/JS/galiris.js
@@ -1,4 +1,5 @@
 $( document ).ready(function(){
+    //Random pick of background image
     var backselect = Math.floor((Math.random() * 3) + 1);
     $('html').css('background-image', 'url(/IMG/BACKGROUND_'+backselect+'.jpg)', 'no-repeat center center fixed');
     $('html').css('background-size', 'cover');
