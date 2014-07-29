a57ed026e3ef20ef7aaf7d765d6aec29d7e7b3cc
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2014-07-29 15:58:02 +0000

    more photos

--- public/JS/galiris.js
+++ public/JS/galiris.js
@@ -1,6 +1,6 @@
 $( document ).ready(function(){
     //Random pick of background image
-    var backselect = Math.floor((Math.random() * 3) + 1);
+    var backselect = Math.floor((Math.random() * 11) + 1);
     $('html').css('background-image', 'url(/IMG/BACKGROUND_'+backselect+'.jpg)', 'no-repeat center center fixed');
     $('html').css('background-size', 'cover');
 });
